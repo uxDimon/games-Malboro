4d521e1133e10092b6d288e66890d9f9e5433b39
Шелков Дмитрий <bobyorrr@gmail.com>
2020-11-11 10:03:06 +0300

    убрал лишние

--- tabak-game/assets/js/main.js
+++ tabak-game/assets/js/main.js
@@ -1,220 +1,218 @@
 $.ube = {
-  host: "https://ube-test.pmsm.org.ru",
-  toggleLoader: function (visible) {
-    /*Можно стилизовать отображение загрузки*/
-    visible ? $(".js-loader").fadeIn() : $(".js-loader").fadeOut();
-  },
-  showPopup: function (message) {
-    showCustomPopup(message);
-  },
-  showRules: function (event) {
-    $('.privacy').addClass('active');
-  },
-  hideRules: function (event) {
-    $('.privacy').removeClass('active');
-  }
-}
+	host: "https://ube-test.pmsm.org.ru",
+	toggleLoader: function (visible) {
+		/*Можно стилизовать отображение загрузки*/
+		visible ? $(".js-loader").fadeIn() : $(".js-loader").fadeOut();
+	},
+	showPopup: function (message) {
+		showCustomPopup(message);
+	},
+	showRules: function (event) {
+		$(".privacy").addClass("active");
+	},
+	hideRules: function (event) {
+		$(".privacy").removeClass("active");
+	},
+};
 
 $(document).ready(function () {
-  //Global Variables
-  var imageArr = [];  //insert all images in this array to preload
-  var timerArray = [50, 100, 80];
-  var mouseDown_timer;
-  var percentageToWin = 99;
-  var maxLeaf = 108;
-  var currentLeaf = 0;
-  var imagesPath = "assets/img/";
-  var percentageHeal = 0;
-  /*var hammertime;
+	//Global Variables
+	var imageArr = []; //insert all images in this array to preload
+	var timerArray = [10, 10, 10];
+	var mouseDown_timer;
+	var percentageToWin = 99;
+	var maxLeaf = 108;
+	var currentLeaf = 0;
+	var imagesPath = "assets/img/";
+	var percentageHeal = 0;
+	/*var hammertime;
   var myElement = document.getElementsByClassName('tutorial-screen');*/
 
-  //to delete
-  $('.wrapper').hide();
+	//to delete
+	$(".wrapper").hide();
 
-  //loading images
-  var Progress = {
-    bar: null,
-    index: 0,
-    imgList: [],
-    init: function () {
-      var box = document.getElementById("progressBox");
-      var prog = document.createElement("progress");
-      box.appendChild(prog);
-      // console.log('-',prog)
-      this.bar = prog;
-    },
+	//loading images
+	var Progress = {
+		bar: null,
+		index: 0,
+		imgList: [],
+		init: function () {
+			var box = document.getElementById("progressBox");
+			var prog = document.createElement("progress");
+			box.appendChild(prog);
+			// console.log('-',prog)
+			this.bar = prog;
+		},
 
-    loadImages: function (paths) {
-      var self = this,
-        i, length = paths.length;
-      // this.bar.max = length;
-      for (i = 0; i < length; i++) {
-        var img = new Image();
-        img.onload = function () {
-          self.increase();
-          if (self.index >= length) {
-            self.done();
-          }
-        };
-        img.src = paths[i];
-        this.imgList.push(img);
-      }
-    },
+		loadImages: function (paths) {
+			var self = this,
+				i,
+				length = paths.length;
+			// this.bar.max = length;
+			for (i = 0; i < length; i++) {
+				var img = new Image();
+				img.onload = function () {
+					self.increase();
+					if (self.index >= length) {
+						self.done();
+					}
+				};
+				img.src = paths[i];
+				this.imgList.push(img);
+			}
+		},
 
-    increase: function () {
-      this.index++;
-      var preloaderNUmb = parseInt((100 / imageArr.length) * this.index);
-      $('.num').text(preloaderNUmb + '%');
-      $(".loading-page hr").css("width", preloaderNUmb + "%");
-    },
+		increase: function () {
+			this.index++;
+			var preloaderNUmb = parseInt((100 / imageArr.length) * this.index);
+			$(".num").text(preloaderNUmb + "%");
+			$(".loading-page hr").css("width", preloaderNUmb + "%");
+		},
 
-    done: function () {
-      // console.log("I am done loading");
-      $('.wrapper-preloader').hide();
-      initGame();
-    }
+		done: function () {
+			// console.log("I am done loading");
+			$(".wrapper-preloader").hide();
+			initGame();
+		},
+	};
 
-  };
+	getAllImages();
+	Progress.loadImages(imageArr);
 
-  getAllImages();
-  Progress.loadImages(imageArr);
+	// Function to generate random number
+	function randomNumber(min, max) {
+		return parseInt(Math.random() * (max - min) + min);
+	}
 
-  // Function to generate random number
-  function randomNumber(min, max) {
-    return parseInt(Math.random() * (max - min) + min);
-  }
+	function getAllImages() {
+		for (var i = 0; i <= maxLeaf; i++) {
+			imageArr.push(imagesPath + "gold-images/leaf_" + i + ".jpg");
+		}
+	}
 
-  function getAllImages() {
-    for (var i = 0; i <= maxLeaf; i++) {
-      imageArr.push(imagesPath + "gold-images/leaf_" + i + ".jpg");
-    }
-  }
+	function initGame() {
+		document.querySelectorAll(".auth-tel").forEach((item) => {
+			let telMask = IMask(item, {
+				mask: "+{7}(000)000-00-00",
+			});
+		});
 
-  function initGame() {
+		$(".wrapper").fadeIn();
+		$(".begin-game").click(function () {
+			$(".screen-1").hide();
+			$(".screen-2").fadeIn();
+			addEventListeners();
 
-    document.querySelectorAll('.auth-tel').forEach(item => {
-      let telMask = IMask(item, {
-        mask: '+{7}(000)000-00-00'
-      });
-    });
+			$(".wrapper").on("mouseleave", function () {
+				resetTimer();
+			});
 
-    $('.wrapper').fadeIn();
-    $('.begin-game').click(function () {
-      $('.screen-1').hide();
-      $('.screen-2').fadeIn();
-      addEventListeners();
+			$(".screen-photo .btn").click(function () {});
 
-      $('.wrapper').on("mouseleave", function () {
-        resetTimer();
-      });
+			$(".try-again").click(function () {
+				currentLeaf = 0;
+				resetTimer();
+				changeLeaf();
+				$(".game-fail").hide();
+				$(".txt-to-hide").show();
+				$(".game-start").fadeIn();
+				$(".container").hide();
+				addEventListeners();
+			});
 
-      $('.screen-photo .btn').click(function () {
+			$(".try-again--secondary").click(function () {
+				currentLeaf = 0;
+				resetTimer();
+				changeLeaf();
+				$(".game-fail").hide();
+				$(".txt-to-hide").show();
+				$(".game-start").fadeIn();
+				$(".game-win").hide();
+				$(".game-after").hide();
+				$(".container").show();
+				addEventListeners();
+			});
+		});
+	}
 
-      });
+	function addEventListeners() {
+		$(".screen-2").bind("mousedown touchstart", function (e) {
+			e.preventDefault();
+			var tempnum = randomNumber(0, timerArray.length);
+			$(".txt-to-hide").fadeOut();
+			$(".container").fadeIn();
+			resetTimer();
+			mouseDown_timer = setInterval(changeLeaf, timerArray[tempnum]);
+			removeEventListeners();
+			$(".screen-2").bind("mouseup touchend", function () {
+				resetTimer();
+				showPercentageHeal();
+			});
+		});
+	}
 
-      $('.try-again').click(function () {
-        currentLeaf = 0;
-        resetTimer();
-        changeLeaf();
-        $('.game-fail').hide();
-        $(".txt-to-hide").show();
-        $('.game-start').fadeIn();
-        $('.container').hide();
-        addEventListeners();
-      });
+	function removeEventListeners() {
+		$(".screen-2").unbind();
+	}
 
-      $('.try-again--secondary').click(function () {
-        currentLeaf = 0;
-        resetTimer();
-        changeLeaf();
-        $('.game-fail').hide();
-        $(".txt-to-hide").show();
-        $('.game-start').fadeIn();
-        $('.game-win').hide();
-        $('.container').show();
-        addEventListeners();
-      });
+	function resetTimer() {
+		clearInterval(mouseDown_timer);
+	}
 
-    });
-  }
+	function calculatePercentageHeal() {
+		percentageHeal = parseInt((100 / maxLeaf) * currentLeaf);
+	}
 
-  function addEventListeners() {
-    $('.screen-2').bind("mousedown touchstart", function (e) {
-      e.preventDefault();
-      var tempnum = randomNumber(0, timerArray.length);
-      $(".txt-to-hide").fadeOut();
-      $(".container").fadeIn();
-      resetTimer();
-      mouseDown_timer = setInterval(changeLeaf, timerArray[tempnum]);
-      removeEventListeners();
-      $('.screen-2').bind("mouseup touchend", function () {
-        resetTimer();
-        showPercentageHeal();
-      });
-    });
-  }
+	function showPercentageHeal() {
+		$(".game-start").hide();
+		removeEventListeners();
+		if (percentageHeal > percentageToWin) {
+			$(".game-win").fadeIn();
+			$(".container").hide();
+			setTimeout(() => {
+				$(".game-win").hide();
+				$(".game-after").fadeIn();
+			}, 3000);
+		} else {
+			$(".calculated-percentage").html(percentageHeal);
+			$(".game-fail").fadeIn();
+		}
+	}
 
-  function removeEventListeners() {
-    $('.screen-2').unbind();
-  }
-
-  function resetTimer() {
-    clearInterval(mouseDown_timer);
-  }
-
-  function calculatePercentageHeal() {
-    percentageHeal = parseInt(100 / maxLeaf * currentLeaf);
-  }
-
-  function showPercentageHeal() {
-    $('.game-start').hide();
-    removeEventListeners();
-    if (percentageHeal > percentageToWin) {
-      $('.game-win').fadeIn();
-      $('.container').hide();
-    }
-    else {
-      $('.calculated-percentage').html(percentageHeal);
-      $('.game-fail').fadeIn();
-    }
-  }
-
-  function changeLeaf() {
-    $('.bg-image img').attr('src', imageArr[currentLeaf]);
-    if (currentLeaf < maxLeaf) {
-      currentLeaf++;
-    }
-    calculatePercentageHeal();
-    $('.progress-bar').css("width", percentageHeal + "%");
-  }
+	function changeLeaf() {
+		$(".bg-image img").attr("src", imageArr[currentLeaf]);
+		if (currentLeaf < maxLeaf) {
+			currentLeaf++;
+		}
+		calculatePercentageHeal();
+		$(".progress-bar").css("width", percentageHeal + "%");
+	}
 });
 
-$(document).on('click', '.screen-photo .btn', function () {
-  $("#scanface").addClass('active');
-  $("#scanface").ubeScanpackFace('scanpack-casual-reg', function () {
-    $("#scanface").removeClass('active');
-    $('.screen-photo').removeClass('active');
-    $('.screen-tel').addClass('active');
-    $('.screen-tel__form').ubeScanpackLogin('scanpack-casual-reg', function () {
-
-    });
-  });
+$(document).on("click", ".screen-photo .btn", function () {
+	$("#scanface").addClass("active");
+	$("#scanface").ubeScanpackFace("scanpack-casual-reg", function () {
+		$("#scanface").removeClass("active");
+		$(".screen-photo").removeClass("active");
+		$(".screen-tel").addClass("active");
+		$(".screen-tel__form").ubeScanpackLogin("scanpack-casual-reg", function () {});
+	});
 });
 
-$(document).on('click', '.privacy__close', function () {
-  $.ube.hideRules();
+$(document).on("click", ".privacy__close", function () {
+	$.ube.hideRules();
 });
 
 $(document).ready(function () {
-  $('.screen-tel__form').ubeScanpackLogin('scanpack-casual-reg', function (result) {
-    if (result === 'loginSuccess') {
-      console.log(1);
-      $('.screen-tel').removeClass('active');
-      $('.screen-denied').addClass('active');
-    } else if (result === 'registered') {
-      console.log(2);
-      $('.screen-tel').removeClass('active');
-      $('.screen-success').addClass('active');
-    }
-  });
-})
+	$(".screen-tel__form").ubeScanpackLogin("scanpack-casual-reg", function (result) {
+		if (result === "loginSuccess") {
+			console.log(1);
+			$(".screen-tel").removeClass("active");
+			$(".screen-denied").addClass("active");
+		} else if (result === "registered") {
+			console.log(2);
+			$(".screen-tel").removeClass("active");
+			$(".screen-success").addClass("active");
+		}
+	});
+});
--- tabak-promo/assets/js/main.js
+++ tabak-promo/assets/js/main.js
@@ -1,71 +1,72 @@
-window.addEventListener('DOMContentLoaded', function () {
-  if (localStorage.getItem('isGameComplete') != true) {
-    $('.screen-photo').addClass('active');
-  } else {
-    $('.screen-2').addClass('active');
-  }
+window.addEventListener("DOMContentLoaded", function () {
+	if (localStorage.getItem("isGameComplete") != true) {
+		$(".screen-photo").addClass("active");
+	} else {
+		$(".screen-2").addClass("active");
+	}
 });
 
-
 $.ube = {
-  host: "https://ube-test.pmsm.org.ru",
-  toggleLoader: function (visible) {
-    /*Можно стилизовать отображение загрузки*/
-    visible ? $(".js-loader").fadeIn() : $(".js-loader").fadeOut();
-  },
-  showPopup: function (message) {
-    showCustomPopup(message);
-  },
-  hidePopup: function () {
-    hideCustomPopup();
-  },
-  showRules: function (event) {
-    $('.privacy').addClass('active');
-  },
-  hideRules: function (event) {
-    $('.privacy').removeClass('active');
-  }
-}
+	host: "https://ube-test.pmsm.org.ru",
+	toggleLoader: function (visible) {
+		/*Можно стилизовать отображение загрузки*/
+		visible ? $(".js-loader").fadeIn() : $(".js-loader").fadeOut();
+	},
+	showPopup: function (message) {
+		showCustomPopup(message);
+	},
+	hidePopup: function () {
+		hideCustomPopup();
+	},
+	showRules: function (event) {
+		$(".privacy").addClass("active");
+	},
+	hideRules: function (event) {
+		$(".privacy").removeClass("active");
+	},
+};
 
 function showCustomPopup(message) {
-  if (message.indexOf('молод') > -1) {
-    $('.popup .text').text('Кажется, ты выглядишь слишком молодо. Возможно, ты не выполнил одно из условий. В любом случае стоит попробовать еще раз, чтобы изображение получилось более чётким, не засвеченным и не слишком затемненным.');
-  } else {
-    $('.popup .text').text(message);
-  }
-  $('.popup').fadeIn();
-  $('.overlay').fadeIn();
+	if (message.indexOf("молод") > -1) {
+		$(".popup .text").text(
+			"Кажется, ты выглядишь слишком молодо. Возможно, ты не выполнил одно из условий. В любом случае стоит попробовать еще раз, чтобы изображение получилось более чётким, не засвеченным и не слишком затемненным."
+		);
+	} else {
+		$(".popup .text").text(message);
+	}
+	$(".popup").fadeIn();
+	$(".overlay").fadeIn();
 }
 
 function hideCustomPopup() {
-  $('.popup').fadeOut();
-  $('.overlay').fadeOut();
+	$(".popup").fadeOut();
+	$(".overlay").fadeOut();
 }
 
-
-$(document).on('click', '.screen-photo .btn', function () {
-  $("#scanface").addClass('active');
-  $("#scanface").ubeScanpackFace('scanpack-casual-reg', function (result) {
-    console.log(result);
-    if (result) {
-      $("#scanface").removeClass('active');
-      $('.screen-photo').removeClass('active');
-      $('.screen-tel').addClass('active');
-      $('.screen-tel__form').ubeScanpackLogin('scanpack-casual-reg', function (result) {
-        if (result === 'loginSuccess') {
-          console.log(1);
-          $('.screen-tel').removeClass('active');
-          $('.screen-denied').addClass('active');
-        } else if (result === 'registered') {
-          console.log(2);
-          $('.screen-tel').removeClass('active');
-          $('.screen-success').addClass('active');
-        }
-      });
-    }
-  });
+$(document).on("click", ".screen-photo .btn", function () {
+	console.log("000");
+	$("#scanface").addClass("active");
+	$("#scanface").ubeScanpackFace("scanpack-casual-reg", function (result) {
+		console.log(result);
+		if (result) {
+			$("#scanface").removeClass("active");
+			$(".screen-photo").removeClass("active");
+			$(".screen-tel").addClass("active");
+			$(".screen-tel__form").ubeScanpackLogin("scanpack-casual-reg", function (result) {
+				if (result === "loginSuccess") {
+					console.log(1);
+					$(".screen-tel").removeClass("active");
+					$(".screen-denied").addClass("active");
+				} else if (result === "registered") {
+					console.log(2);
+					$(".screen-tel").removeClass("active");
+					$(".screen-success").addClass("active");
+				}
+			});
+		}
+	});
 });
 
-$(document).on('click', '.privacy__close', function () {
-  $.ube.hideRules();
+$(document).on("click", ".privacy__close", function () {
+	$.ube.hideRules();
 });
